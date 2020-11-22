8871e466b89b0d717ef13de43d68541322b10ca0
Alex Katlein <dev@vemilyus.com>
2020-11-22 16:57:08 +0100

    Made implementation of DefaultEventEmitter thread-safe
    - Reformatted code using Kotlin style
    - Updated dependencies, especially Kotlin
    - Updated plugins
    - Changed license to BSD 3-clause

--- src/horus/events/DefaultEventEmitter.kt
+++ src/horus/events/DefaultEventEmitter.kt
@@ -1,21 +1,39 @@
 /**
- * Copyright 2020 The Horus Events Authors
+ * BSD 3-Clause License
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Copyright (c) 2020, Alex Katlein
+ * All rights reserved.
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 package horus.events
 
 import org.slf4j.LoggerFactory
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.ConcurrentLinkedQueue
 
 /**
  * This is the default implementation of an event emitter which can be extended
@@ -31,12 +49,26 @@ import org.slf4j.LoggerFactory
 open class DefaultEventEmitter(private val failFast: Boolean = false) : EventEmitter {
     private val log = LoggerFactory.getLogger(javaClass)!!
 
-    private val listeners = mutableMapOf<EventKey<*>, MutableList<suspend (Any) -> Unit>>()
-    private val listenersOnce = mutableMapOf<EventKey<*>, MutableList<suspend (Any) -> Unit>>()
+    private val listeners =
+        ConcurrentHashMap<EventKey<*>, ConcurrentLinkedQueue<suspend (Any) -> Unit>>()
 
-    @Suppress("LongMethod", "TooGenericExceptionCaught")
+    private val listenersOnce =
+        ConcurrentHashMap<EventKey<*>, ConcurrentLinkedQueue<suspend (Any) -> Unit>>()
+
     override suspend fun <T : Any> emit(key: EventKey<T>, payload: T) {
-        listeners[key]?.forEach {
+        listeners[key]?.toList()?.callAll(payload)
+
+        listenersOnce[key]?.let { listenersQueue ->
+            val listeners = listenersQueue.toList()
+            listenersQueue.clear()
+
+            listeners.callAll(payload)
+        }
+    }
+
+    private suspend fun Iterable<suspend (Any) -> Unit>.callAll(payload: Any) =
+        forEach {
+            @Suppress("TooGenericExceptionCaught")
             try {
                 it(payload)
             } catch (x: Exception) {
@@ -47,40 +79,39 @@ open class DefaultEventEmitter(private val failFast: Boolean = false) : EventEmi
             }
         }
 
-        listenersOnce[key]?.onEach {
-            try {
-                it(payload)
-            } catch (x: Exception) {
-                if (failFast)
-                    throw x
-                else
-                    log.warn("Caught exception in listener", x)
-            }
-        }?.removeAll { true }
-    }
-
     override fun <T : Any> on(key: EventKey<T>, block: suspend (T) -> Unit) {
         @Suppress("UNCHECKED_CAST")
-        listeners.computeIfAbsent(key) { mutableListOf() }.add(block as (suspend (Any) -> Unit))
+        listeners
+            .computeIfAbsent(key) { ConcurrentLinkedQueue() }
+            .add(block as (suspend (Any) -> Unit))
     }
 
     override fun <T : Any> once(key: EventKey<T>, block: suspend (T) -> Unit) {
         @Suppress("UNCHECKED_CAST")
-        listenersOnce.computeIfAbsent(key) { mutableListOf() }.add(block as (suspend (Any) -> Unit))
+        listenersOnce
+            .computeIfAbsent(key) { ConcurrentLinkedQueue() }
+            .add(block as (suspend (Any) -> Unit))
     }
 
     override fun clear(key: EventKey<*>?) {
         if (key != null) {
-            listeners[key]?.removeAll { true }
-            listenersOnce[key]?.removeAll { true }
+            listeners.remove(key)
+            listenersOnce.remove(key)
         } else {
-            listeners.forEach { it.value.removeAll { true } }
-            listenersOnce.forEach { it.value.removeAll { true } }
+            listeners.clear()
+            listenersOnce.clear()
         }
     }
 
     override fun <T : Any> remove(listener: suspend (T) -> Unit) {
-        listeners.forEach { it.value.remove(listener) }
-        listenersOnce.forEach { it.value.remove(listener) }
+        listeners.entries.removeIf { (_, listeners) ->
+            listeners.remove(listener)
+            listeners.isEmpty()
+        }
+
+        listenersOnce.entries.removeIf { (_, listeners) ->
+            listeners.remove(listener)
+            listeners.isEmpty()
+        }
     }
 }
